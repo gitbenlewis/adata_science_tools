46e547dc6716065b133a34641e352cab0c71119d
blewis <105738044+gitbenlewis@users.noreply.github.com>
2025-01-25 00:30:58 +0000

    set up basic package structure

---
+++ __init__.py
@@ -0,0 +1,7 @@
+"""  Bens anndata data science tools ... """
+
+from . import anndata_io as adio
+from . import anndata_preprocessing as adpp
+from . import anndata_plotting as adpl
+from . import anndata_tools as adtl
+
