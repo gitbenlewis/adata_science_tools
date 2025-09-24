a662dbee362cb9a160f2270ca252be9a4d9810af
blewis <benlewis.biology@gmail.com>
2025-09-24 23:27:48 +0000

    added corr_dotplot

--- anndata_plotting/_plots.py
+++ anndata_plotting/_plots.py
@@ -1,57 +1,7 @@
+''' plotting functions for anndata data science tools '''
+# module level imports
 import matplotlib.pyplot as plt
 
-# Paul Tol’s 10-color set # https://sronpersonalpages.nl/~pault/
-tol_colors = [
-    "#332288", "#88CCEE", "#44AA99", "#117733",
-    "#999933", "#DDCC77", "#661100", "#CC6677",
-    "#882255", "#AA4499"
-]
-
-
-
-
-def show_tol_colors(colors=None):
-    """
-    Creates a bar plot where each bar has one of the given colors.
-    The x-axis is labeled with the hex color codes.
-    """
-    import matplotlib.pyplot as plt
-    if colors is None:
-        tol_colors = [
-    "#332288", "#88CCEE", "#44AA99", "#117733",
-    "#999933", "#DDCC77", "#661100", "#CC6677",
-    "#882255", "#AA4499"
-        ]
-        colors=tol_colors
-    n = len(colors)
-    x_vals = range(n)
-    y_vals = [1]*n  # All bars have the same height (1)
-
-    fig, ax = plt.subplots(figsize=(8, 2))
-    bars = ax.bar(x_vals, y_vals)
-
-    # Set each bar’s color and label
-    for i, bar in enumerate(bars):
-        bar.set_color(colors[i])
-        # Put the hex code as an x-axis tick label
-        ax.text(
-            i, 0.5, colors[i],
-            rotation=90, fontsize=9,
-            color='white', ha='center', va='center',
-            bbox=dict(facecolor='black', alpha=0.3, boxstyle='round')
-        )
-
-    # Remove extra chart details
-    ax.set_xticks(x_vals)
-    ax.set_xticklabels(['']*n)   # we place color codes in the bars, so x tick labels can be blank
-    ax.set_yticks([])
-
-    ax.set_xlim(-0.5, n - 0.5)
-    ax.set_ylim(0, 1)
-
-    ax.set_title("Paul Tol's 10-Color Palette", fontsize=12)
-    plt.tight_layout()
-    plt.show()
 
 
 #######  START  ############. Volcano plots ###################.###################.###################.###################.
@@ -61,7 +11,7 @@ def show_tol_colors(colors=None):
 # Todo add accept adata or df input
 # 2025.08.22 updated to re org hue / label logic and add horizontal pvalue threshold to match updates elsewhere 
 # 2025.02.28 updated to used the hue_column to set the hue values 
-def volcano_plot_sns_single_comparison_generic(
+def volcano_plot_generic(
         _df, 
         l2fc_col: str | None = 'log2FoldChange',
         set_xlabel: str | None = 'log2fc model',
