04ccbbff610bca0313d6a88e5333dec6af37af49
blewis <benlewis.biology@gmail.com>
2025-09-24 23:27:48 +0000

    added corr_dotplot

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 """  Bens anndata data science tools ... """
 
-from . import anndata_io as adio
-from . import anndata_preprocessing as adpp
-from . import anndata_plotting as adpl
-from . import anndata_tools as adtools
+from . import anndata_io as io
+from . import anndata_preprocessing as pp
+from . import anndata_plotting as pl
+from . import anndata_tools as tl
 
 from .anndata_io import *
 from .anndata_preprocessing import *
--- anndata_plotting/__init__.py
+++ anndata_plotting/__init__.py
@@ -1,7 +1,10 @@
 '''
 the plotting subpackage
 '''
-
+from ._utils import *
 from ._plots import *
 from ._column_plots import *
+from ._row_plots import *
+from ._corr_dotplots import *
+from . import palettes
 from ._plots_depreciated import *
