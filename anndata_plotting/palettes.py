f4359f798150a680d342cf1184159032a5104dc8
Benjamin Mark Lewis <benlewis.biology@gmail.com>
2025-12-02 18:53:32 +0000

    corr plots legend named

--- anndata_plotting/palettes.py
+++ anndata_plotting/palettes.py
@@ -9,6 +9,7 @@ tol_colors = [
     "#882255", "#AA4499"
 ]
 
+Iridescent = ['#FEFBE9', '#FCF7D5', '#F5F3C1', '#EAF0B5', '#DDECBF', '#D0E7CA', '#C2E3D2', '#B5DDD8', '#A8D8DC', '#9BD2E1', '#8DCBE4', '#81C4E7', '#7BBCE7', '#7EB2E4', '#88A5DD', '#9398D2', '#9B8AC4', '#9D7DB2', '#9A709E', '#906388', '#805770', '#684957', '#46353A']
 
 ######## Taken directly from scanpy ..... 
 
